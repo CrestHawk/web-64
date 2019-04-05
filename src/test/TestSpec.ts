0f1769cfe2e64fa2d8a994dc2bd74b89804d2a8a
CrestHawk <emailtomdavies@gmail.com>
2019-04-05 23:07:20 +0000

    gulpfile setup for running unit tests

---
+++ src/test/TestSpec.ts
@@ -0,0 +1,12 @@
+import chai = require("chai");
+
+describe("test", () =>
+{
+    describe("When I run the tests", () =>
+    {
+        it("They should pass", () =>
+        {
+            chai.expect(true).to.equal(true);
+        });
+    });
+});
